a89bd885b867b65cb7db3fcad4635cd38cba2227
drlkf <drlkf@drlkf.net>
2024-05-13 20:18:09 +0200

    feat: add duration parsers

--- src/Text/Megaparsec/Time.hs
+++ src/Text/Megaparsec/Time.hs
@@ -2,16 +2,25 @@
 
 module Text.Megaparsec.Time
   ( dayParser
+  , durationParser
+  , hoursParser
+  , minutesParser
+  , secondsParser
   , timeParser
   ) where
 
+import           Control.Applicative       (optional, (<|>))
 import           Control.Monad             (replicateM, void)
 import           Control.Monad.Combinators (choice, some)
 import           Data.Functor              (($>))
-import           Data.Time                 (DayOfWeek (..), TimeOfDay (..))
+import           Data.Maybe                (fromMaybe)
+import           Data.Time                 (DayOfWeek (..), NominalDiffTime,
+                                            TimeOfDay (..),
+                                            secondsToNominalDiffTime)
 import           Data.Void                 (Void)
 import           Text.Megaparsec           (Parsec, try)
-import           Text.Megaparsec.Char      (char, digitChar, string')
+import           Text.Megaparsec.Char      (char, digitChar, space, string')
+import           Text.Megaparsec.Utils     (posNumParser)
 
 dayParser :: Parsec Void String (Either Int DayOfWeek)
 dayParser = choice
@@ -28,6 +37,30 @@ dayParser = choice
         absoluteDay = toEnum . read <$> try (some digitChar)
         relativeDay = char '+' >> read <$> try (some digitChar)
 
+durationParser :: Parsec Void String NominalDiffTime
+durationParser = try hours <|> try minutes <|> secondsParser
+  where hours = do
+          h <- hoursParser <* space
+          m <- fromMaybe zero <$> optional (try minutes)
+          s <- fromMaybe zero <$> optional secondsParser
+
+          return $ h + m + s
+
+        minutes = do
+          m <- minutesParser <* space
+          s <- fromMaybe zero <$> optional secondsParser
+
+          return $ m + s
+
+hoursParser :: Parsec Void String NominalDiffTime
+hoursParser = secondsToNominalDiffTime . (* 3600) <$> posNumParser <* char 'h'
+
+minutesParser :: Parsec Void String NominalDiffTime
+minutesParser = secondsToNominalDiffTime . (* 60) <$> posNumParser <* char 'm'
+
+secondsParser :: Parsec Void String NominalDiffTime
+secondsParser = secondsToNominalDiffTime <$> posNumParser <* optional (char 's')
+
 timeParser :: Parsec Void String TimeOfDay
 timeParser = do
   h <- read <$> replicateM 2 digitChar
@@ -35,3 +68,6 @@ timeParser = do
   m <- read <$> replicateM 2 digitChar
 
   return $ TimeOfDay h m 0
+
+zero :: NominalDiffTime
+zero = secondsToNominalDiffTime 0
--- test/Text/Megaparsec/TimeSpec.hs
+++ test/Text/Megaparsec/TimeSpec.hs
@@ -5,26 +5,43 @@ module Text.Megaparsec.TimeSpec
   ) where
 
 import           Control.Monad        (forM_)
+import           Data.Bifunctor       (Bifunctor (first))
 import           Data.Either          (isLeft)
 import           Data.List.Extra      (lower)
 import           Data.Time            (DayOfWeek (..), TimeOfDay (..),
-                                       defaultTimeLocale, formatTime)
+                                       defaultTimeLocale, formatTime,
+                                       secondsToNominalDiffTime)
+import           Data.Void            (Void)
 import           Test.Hspec           (Spec, context, describe, it, shouldBe,
                                        shouldSatisfy)
-import           Test.QuickCheck      (Arbitrary (..), elements, forAll,
+import           Test.QuickCheck      (Arbitrary (..), Gen, elements, forAll,
                                        property, suchThat)
-import           Text.Megaparsec      (runParser)
-import           Text.Megaparsec.Time (dayParser, timeParser)
+import           Text.Megaparsec      (Parsec, errorBundlePretty, runParser)
+import           Text.Megaparsec.Time (dayParser, durationParser, hoursParser,
+                                       minutesParser, secondsParser, timeParser)
 import           Text.Printf          (printf)
 
 instance Arbitrary TimeOfDay where
-  arbitrary = TimeOfDay
-    <$> ((`mod` 24) . abs <$> arbitrary)
+  arbitrary =
+    (TimeOfDay . (`mod` 24) . abs
+     <$> arbitrary)
     <*> ((`mod` 60) . abs <$> arbitrary)
     <*> pure 0
 
 instance Arbitrary DayOfWeek where
   arbitrary = elements [Monday .. Sunday]
+
+positive
+  :: Num a
+  => Arbitrary a
+  => Gen a
+positive = abs <$> arbitrary
+
+parseOrPrettyError
+  :: Parsec Void String a
+  -> String
+  -> Either String a
+parseOrPrettyError p = first errorBundlePretty . runParser p "test"
 
 spec :: Spec
 spec = do
@@ -37,31 +54,81 @@ spec = do
       ] $ \(title, format) ->
       context title $ do
       it "nominal" . forAll (arbitrary `suchThat` weekday) $ \d ->
-        runParser dayParser "day" (formatTime defaultTimeLocale format d) `shouldBe`
+        parseOrPrettyError dayParser (formatTime defaultTimeLocale format d) `shouldBe`
         Right (Right d)
 
       it "lowercase" . forAll (arbitrary `suchThat` weekday) $ \d ->
-        runParser dayParser "day" (lower (formatTime defaultTimeLocale format d)) `shouldBe`
+        parseOrPrettyError dayParser (lower (formatTime defaultTimeLocale format d)) `shouldBe`
         Right (Right d)
 
       it "weekend" . forAll (arbitrary `suchThat` (not . weekday)) $ \d ->
-        runParser dayParser "day" (formatTime defaultTimeLocale format d) `shouldSatisfy`
+        parseOrPrettyError dayParser (formatTime defaultTimeLocale format d) `shouldSatisfy`
         isLeft
 
     context "tomorrow" $ do
       it "capitalized" $
-        runParser dayParser "day" "Tomorrow "`shouldBe` Right (Left 1)
+        parseOrPrettyError dayParser "Tomorrow "`shouldBe` Right (Left 1)
 
       it "lowercase" $
-        runParser dayParser "day" "tomorrow "`shouldBe` Right (Left 1)
+        parseOrPrettyError dayParser "tomorrow "`shouldBe` Right (Left 1)
 
     it "future day" . forAll (abs <$> arbitrary) $ \x ->
-      runParser dayParser "day" (printf "+%d" x) `shouldBe` Right (Left x)
+      parseOrPrettyError dayParser (printf "+%d" x) `shouldBe` Right (Left x)
 
     it "invalid" . forAll (negate . (+1) . abs <$> arbitrary) $ \x ->
-      runParser dayParser "day" (printf "+%d" (x :: Int)) `shouldSatisfy` isLeft
+      parseOrPrettyError dayParser (printf "+%d" (x :: Int)) `shouldSatisfy` isLeft
+
+  describe "duration" $ do
+    it "hours" . forAll positive $ \h ->
+      parseOrPrettyError hoursParser (printf "%dh" h) `shouldBe`
+      Right (secondsToNominalDiffTime (fromInteger h * 3600))
+
+    it "minutes" . forAll positive $ \m ->
+      parseOrPrettyError minutesParser (printf "%dm" m) `shouldBe`
+      Right (secondsToNominalDiffTime (fromInteger m * 60))
+
+    context "seconds" $ do
+      it "raw" . forAll positive $ \s ->
+        parseOrPrettyError secondsParser (printf "%d" s) `shouldBe`
+        Right (secondsToNominalDiffTime (fromInteger s))
+
+      it "with suffix" . forAll positive $ \s ->
+        parseOrPrettyError secondsParser (printf "%ds" s) `shouldBe`
+        Right (secondsToNominalDiffTime (fromInteger s))
+
+    context "duration" $ do
+      context "hours" $ do
+        it "hms" . forAll ((,,) <$> positive <*> positive <*> positive) $ \(h, m, s) ->
+          parseOrPrettyError durationParser (printf "%dh %dm %ds" h m s) `shouldBe`
+          Right (secondsToNominalDiffTime (fromInteger (((h * 60 + m) * 60) + s)))
+
+        it "hm" . forAll ((,) <$> positive <*> positive) $ \(h, m) ->
+          parseOrPrettyError durationParser (printf "%dh %dm" h m) `shouldBe`
+          Right (secondsToNominalDiffTime (fromInteger ((h * 60 + m) * 60)))
+
+        it "hs" . forAll ((,) <$> positive <*> positive) $ \(h, s) ->
+          parseOrPrettyError durationParser (printf "%dh %ds" h s) `shouldBe`
+          Right (secondsToNominalDiffTime (fromInteger (h * 3600 + s)))
+
+      context "minutes" $ do
+        it "ms" . forAll ((,) <$> positive <*> positive) $ \(m, s) ->
+          parseOrPrettyError durationParser (printf "%dm%ds" m s) `shouldBe`
+          Right (secondsToNominalDiffTime (fromInteger (m * 60 + s)))
+
+        it "m" . forAll positive $ \m ->
+          parseOrPrettyError durationParser (printf "%dm" m) `shouldBe`
+          Right (secondsToNominalDiffTime (fromInteger (m * 60)))
+
+      context "seconds" $ do
+        it "s" . forAll positive $ \s ->
+          parseOrPrettyError durationParser (printf "%ds" s) `shouldBe`
+          Right (secondsToNominalDiffTime (fromInteger s))
+
+        it "no suffix" . forAll positive $ \s ->
+          parseOrPrettyError durationParser (show s) `shouldBe`
+          Right (secondsToNominalDiffTime (fromInteger s))
 
   describe "time" $ do
     it "valid" . property $ \t ->
-      runParser timeParser "time" (formatTime defaultTimeLocale "%R" t) `shouldBe`
+      parseOrPrettyError timeParser (formatTime defaultTimeLocale "%R" t) `shouldBe`
       Right t
