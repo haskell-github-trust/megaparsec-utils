02c61d97f567563e5f13c8df5e56b8350e6482fc
drlkf <drlkf@drlkf.net>
2025-07-07 20:11:39 +0200

    ci: add github workflows

---
+++ .releaserc.mjs
@@ -0,0 +1,41 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    branches: ["master"],
+    tagFormat: "${version}",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "semantic-release-mirror-version",
+            {
+                "fileGlob": "@(package.yaml|megaparsec-utils.cabal)",
+                "placeholderRegExp": "0.0.0-dev"
+            }
+        ],
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md",
+                changelogTitle: "Changelog"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: [
+                    "CHANGELOG.md",
+                ]
+            }
+        ],
+        "@semantic-release/github",
+        [
+            "semantic-release-hackage",
+            {
+                "versionPrefix": "0.",
+                "publishDocumentation": true
+            }
+        ]
+    ]
+}
